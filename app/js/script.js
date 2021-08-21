0513db39adc7faf483dbf7296914ad2d217e39af
tomohiro ibi <68018348+ibitomohiro@users.noreply.github.com>
2021-08-21 23:08:53 +0900

    ハンバーガーメニュー修正

--- app/js/script.js
+++ app/js/script.js
@@ -1,10 +1,13 @@
 const btnHamburger = document.querySelector('#btnHamburger');
+const header = document.querySelector('.header');
+
+
 btnHamburger.addEventListener('click', function(){
   console.log('btn clicked!');
-  if(btnHamburger.classList.contains('open')){
-    btnHamburger.classList.remove('open');
+  if(header.classList.contains('open')){
+    header.classList.remove('open');
   }
   else {
-    btnHamburger.classList.add('open');
+    header.classList.add('open');
   }
 });
