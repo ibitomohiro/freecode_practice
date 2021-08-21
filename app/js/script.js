49c040aa195f42eb89bc29d82cc8dabb6cfaa9c9
tomohiro ibi <68018348+ibitomohiro@users.noreply.github.com>
2021-08-22 01:29:06 +0900

    ovelayのパート 1:18:03

--- app/js/script.js
+++ app/js/script.js
@@ -1,13 +1,19 @@
 const btnHamburger = document.querySelector('#btnHamburger');
 const header = document.querySelector('.header');
+const overlay = document.querySelector('overlay');
 
 
 btnHamburger.addEventListener('click', function(){
   console.log('btn clicked!');
+
   if(header.classList.contains('open')){
     header.classList.remove('open');
+    overlay.classList.remove('fade-in');
+    overlay.classList.add('fade-out');
   }
   else {
     header.classList.add('open');
+    overlay.classList.remove('fade-out');
+    overlay.classList.add('fade-in');
   }
 });
